3474f78bec3175233db7691d280ab2a89cc69abb
aston00 <jacksterwow@gmail.com>
2018-06-28 18:47:57 +0300

    changed comment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function checkAndBuild(start, end, num, str) {
-    //Length wil always be number given multiple 2(two brackets == 1 digit)
+    //Length wil always be given number multiple 2(two brackets == 1 digit)
     str.length == 2 * num && console.log(str);
     start >= end && start != num && checkAndBuild(start + 1, end, num, str + '[');
     end < start && checkAndBuild(start, end + 1, num, str + ']');
