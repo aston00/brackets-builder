92af093385cc76cada695591a994d2d9a8cd15b6
aston00 <jacksterwow@gmail.com>
2018-06-28 18:43:45 +0300

    Added code for brackets building

---
+++ app.js
@@ -0,0 +1,14 @@
+function checkAndBuild(start, end, num, str) {
+    //Length wil always be number given multiple 2(two brackets == 1 digit)
+    str.length == 2 * num && console.log(str);
+    start >= end && start != num && checkAndBuild(start + 1, end, num, str + '[');
+    end < start && checkAndBuild(start, end + 1, num, str + ']');
+}
+
+function init(num) {
+    let start = 0;
+    end = 0;
+    str = '';
+    checkAndBuild(start, end, num, str);
+}
+init(4);
